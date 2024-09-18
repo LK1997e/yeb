b4e836b3aa2f5575e47a81bed8b3dfbb0cd4577f
Admin <2501317959@qq.com>
2024-09-18 12:13:46 +0800

    智能办公系统git提交

--- yeb-server/src/main/java/com/example/server/util/OssUploadUtil.java
+++ yeb-server/src/main/java/com/example/server/util/OssUploadUtil.java
@@ -10,39 +10,40 @@ import java.util.UUID;
 
 public class OssUploadUtil {
     public static String uploadFile(MultipartFile multipartFile){
-        String endpoint = "";
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "";
-        String accessKeySecret = "";
-        // 填写Bucket名称
-        String bucketName = "";
-
-
-        OSS ossClient = null;
-        try {
-
-            // 创建OSSClient实例。
-            ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-            //获取文件上传的流
-            InputStream inputStream = multipartFile.getInputStream();
-            //构建日期目录
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-            String datePath = dateFormat.format(new Date());
-            String originalName = multipartFile.getOriginalFilename();
-            String fileName = UUID.randomUUID().toString();
-            String suffix = originalName.substring(originalName.lastIndexOf("."));
-            String newName = fileName + suffix;
-            String fileUrl = datePath + "/" + newName;
-            ossClient.putObject(bucketName, fileUrl, inputStream);
-            return "https://"+bucketName+"."+endpoint+"/"+fileUrl;
-        } catch(Exception e){
-            e.printStackTrace();
-            return null;
-        }
-        finally {
-            if (ossClient != null) {
-                ossClient.shutdown();
-            }
-        }
+        return null;
+//        String endpoint = "";
+//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "";
+//        String accessKeySecret = "";
+//        // 填写Bucket名称
+//        String bucketName = "";
+//
+//
+//        OSS ossClient = null;
+//        try {
+//
+//            // 创建OSSClient实例。
+//            ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//            //获取文件上传的流
+//            InputStream inputStream = multipartFile.getInputStream();
+//            //构建日期目录
+//            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+//            String datePath = dateFormat.format(new Date());
+//            String originalName = multipartFile.getOriginalFilename();
+//            String fileName = UUID.randomUUID().toString();
+//            String suffix = originalName.substring(originalName.lastIndexOf("."));
+//            String newName = fileName + suffix;
+//            String fileUrl = datePath + "/" + newName;
+//            ossClient.putObject(bucketName, fileUrl, inputStream);
+//            return "https://"+bucketName+"."+endpoint+"/"+fileUrl;
+//        } catch(Exception e){
+//            e.printStackTrace();
+//            return null;
+//        }
+//        finally {
+//            if (ossClient != null) {
+//                ossClient.shutdown();
+//            }
+//        }
     }
 }
